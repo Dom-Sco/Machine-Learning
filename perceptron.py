5e7b1fb8d6c9dff7e9fbc3937d0d5fe4bc3bdc07
Dominic Scocchera <109325886+Dom-Sco@users.noreply.github.com>
2022-12-28 05:04:15 +0000

    Add files via upload

---
+++ perceptron.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+def perceptron(w, x, y, lr, maxiter):
+    j = 0
+    while (j < maxiter):
+        misscnt = 0
+        output = np.sign(x @ w)
+        for i in range(len(output)):
+            if (output[i]!=y[i]):
+                w  = w + lr * y[i] * x[i]
+                misscnt += 1 
+        if (misscnt == 0):
+            break
+        j += 1 
+    return [w, j, misscnt]
+    
+#for most logic functions    
+w = np.array([0,0,0])
+data = np.array([[1,0,0],
+                 [1,0,1],
+                 [1,1,0],
+                 [1,1,1]])
+#AND
+y_and = np.array([-1,-1,-1,1]) #-1 if output is in lower, 1 if in upper
+w_and = perceptron(w, data, y_and, 0.2, 100)
+#OR
+y_or = np.array([-1,1,1,1]) #-1 if output is in lower, 1 if in upper
+w_or = perceptron(w, data, y_or, 0.2, 100)
+#NAND
+y_nand = np.array([1,1,1,-1]) #-1 if output is in lower, 1 if in upper
+w_nand = perceptron(w, data, y_nand, 0.2, 100)
+#NOR
+y_nor = np.array([1,-1,-1,-1]) #-1 if output is in lower, 1 if in upper
+w_nor = perceptron(w, data, y_nor, 0.2, 100)
+
+
+
+
+
+
+#for xor
+w = np.array([0,0,0,0])
+data = np.array([[1,0,0],
+                 [1,0,1],
+                 [1,1,0],
+                 [1,1,1]])
+y = np.array([1,-1,-1,1]) #-1 if output is in lower, 1 if in upper
+
+kernData = np.hstack((data,np.abs(data[:,1]-data[:,2]).reshape(4,1)))
+
+w = perceptron(w, kernData, y, 0.2, 100)
+print(w)
+
+#Computer with perceptrons
+class Computer():
+    def OR(x):
+        return np.sign(np.dot(x,w_or))
+    def AND(x):
+        return np.sign(np.dot(x,w_and))
+    def NAND(x):
+        return np.sign(np.dot(x,w_nand))
+    def NOR(x):
+        return np.sign(np.dot(x,w_nor))
