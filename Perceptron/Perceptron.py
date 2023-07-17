bae8b907d3bc331d1a9d6f53a0cfbd5656ab5756
Dominic Scocchera <dom.scocchera@gmail.com>
2023-07-17 16:51:55 +1000

    Update Perceptron.py
    added plots for functions that the Perceptron learns

--- Perceptron/Perceptron.py
+++ Perceptron/Perceptron.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def perceptron(w, x, y, lr, maxiter):
     j = 0
@@ -33,11 +35,6 @@ w_nand = perceptron(w, data, y_nand, 0.2, 100)
 y_nor = np.array([1,-1,-1,-1]) #-1 if output is in lower, 1 if in upper
 w_nor = perceptron(w, data, y_nor, 0.2, 100)
 
-
-
-
-
-
 #XOR
 w = np.array([0,0,0,0])
 data = np.array([[1,0,0],
@@ -50,3 +47,127 @@ y = np.array([1,-1,-1,1]) #-1 if output is in lower, 1 if in upper
 kernData = np.hstack((data,np.abs(data[:,1]-data[:,2]).reshape(4,1)))
 
 w = perceptron(w, kernData, y, 0.2, 100)
+
+#plots for logic functions
+
+fig, axs = plt.subplots(2, 2)
+
+#AND
+X1 = np.array([0,0,1])
+Y1 = np.array([0,1,0])
+X2 = np.array([1])
+Y2 = np.array([1])
+axs[0,0].scatter(X1,Y1, c = "blue", label="0")
+axs[0,0].scatter(X2,Y2, c = "red", label="1")
+axs[0,0].set_title("And Function")
+axs[0,0].legend()
+
+#OR
+X1 = np.array([0])
+Y1 = np.array([0])
+X2 = np.array([0,1,1])
+Y2 = np.array([1,0,1])
+axs[0,1].scatter(X1,Y1, c = "blue", label="0")
+axs[0,1].scatter(X2,Y2, c = "red", label="1")
+axs[0,1].set_title("Or Function")
+axs[0,1].legend()
+
+#NAND
+X2 = np.array([0,0,1])
+Y2 = np.array([0,1,0])
+X1 = np.array([1])
+Y1 = np.array([1])
+axs[1,0].scatter(X1,Y1, c = "blue", label="0")
+axs[1,0].scatter(X2,Y2, c = "red", label="1")
+axs[1,0].set_title("Nand Function")
+axs[1,0].legend()
+
+#NOR
+X2 = np.array([0])
+Y2 = np.array([0])
+X1 = np.array([0,1,1])
+Y1 = np.array([1,0,1])
+axs[1,1].scatter(X1,Y1, c = "blue", label="0")
+axs[1,1].scatter(X2,Y2, c = "red", label="1")
+axs[1,1].set_title("Nor Function")
+axs[1,1].legend()
+plt.show()
+
+#plots with decision boundary
+
+fig, axs = plt.subplots(2, 2)
+
+#AND
+X1 = np.array([0,0,1])
+Y1 = np.array([0,1,0])
+X2 = np.array([1])
+Y2 = np.array([1])
+axs[0,0].set_xlim(-0.5, 1.5), axs[0,0].set_ylim(-0.5, 1.5)
+axs[0,0].scatter(X1,Y1, c = "blue", label="0")
+axs[0,0].scatter(X2,Y2, c = "red", label="1")
+axs[0,0].axline((0,(-w_and[0][0]-w_and[0][1]*0)/w_and[0][2]) ,(1 ,(-w_and[0][0]-w_and[0][1]*1)/w_and[0][2]))
+axs[0,0].set_title("And Function")
+axs[0,0].legend()
+
+#OR
+X1 = np.array([0])
+Y1 = np.array([0])
+X2 = np.array([0,1,1])
+Y2 = np.array([1,0,1])
+axs[0,1].set_xlim(-0.5, 1.5), axs[0,1].set_ylim(-0.5, 1.5)
+axs[0,1].scatter(X1,Y1, c = "blue", label="0")
+axs[0,1].scatter(X2,Y2, c = "red", label="1")
+axs[0,1].axline((0,(-w_or[0][0]-w_or[0][1]*0)/w_or[0][2]) ,(1 ,(-w_or[0][0]-w_or[0][1]*1)/w_or[0][2]))
+axs[0,1].set_title("Or Function")
+axs[0,1].legend()
+
+#NAND
+X2 = np.array([0,0,1])
+Y2 = np.array([0,1,0])
+X1 = np.array([1])
+Y1 = np.array([1])
+axs[1,0].set_xlim(-0.5, 1.5), axs[1,0].set_ylim(-0.5, 1.5)
+axs[1,0].scatter(X1,Y1, c = "blue", label="0")
+axs[1,0].scatter(X2,Y2, c = "red", label="1")
+axs[1,0].axline((0,(-w_nand[0][0]-w_nand[0][1]*0)/w_nand[0][2]) ,(1 ,(-w_nand[0][0]-w_nand[0][1]*1)/w_nand[0][2]))
+axs[1,0].set_title("Nand Function")
+axs[1,0].legend()
+
+#NOR
+X2 = np.array([0])
+Y2 = np.array([0])
+X1 = np.array([0,1,1])
+Y1 = np.array([1,0,1])
+axs[1,1].set_xlim(-0.5, 1.5), axs[1,1].set_ylim(-0.5, 1.5)
+axs[1,1].scatter(X1,Y1, c = "blue", label="0")
+axs[1,1].scatter(X2,Y2, c = "red", label="1")
+axs[1,1].axline((0,(-w_nor[0][0]-w_nor[0][1]*0)/w_nor[0][2]) ,(1 ,(-w_nor[0][0]-w_nor[0][1]*1)/w_nor[0][2]))
+axs[1,1].set_title("Nor Function")
+axs[1,1].legend()
+plt.show()
+
+
+
+#XOR
+fig = plt.figure()
+X1 = np.array([0,1])
+Y1 = np.array([0,1])
+X2 = np.array([0,1])
+Y2 = np.array([1,0])
+plt.scatter(X1,Y1, c = "blue", label="0")
+plt.scatter(X2,Y2, c = "red", label="1")
+plt.title("Xor Function")
+plt.legend()
+plt.show()
+
+
+fig = plt.figure()
+ax = plt.axes(projection ='3d')
+(X, Y) = np.meshgrid(np.linspace(-0.5,1.5,200),np.linspace(-0.5,1.5,200))
+Z = -(w[0][0]+w[0][1]*X+w[0][2]*Y)/w[0][3];
+ax.scatter(kernData[:,1][1:3], kernData[:,2][1:3], kernData[:,3][1:3], c = "red", label = "1")
+ax.scatter(np.array([kernData[:,1][0],kernData[:,1][3]]),np.array([kernData[:,2][0],kernData[:,2][3]]),np.array([kernData[:,3][0],kernData[:,3][3]]), c = "green", label = "0")
+ax.plot_surface(X, Y, Z)
+plt.legend()
+plt.title("Xor with Kernel Trick")
+plt.show()
