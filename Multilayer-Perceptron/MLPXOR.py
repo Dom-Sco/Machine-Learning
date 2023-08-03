2781bbd0a04810b2a04a5db73be93fb13a91a356
Dominic Scocchera <dom.scocchera@gmail.com>
2023-08-03 14:03:02 +1000

    Update MLPXOR.py

--- Multilayer-Perceptron/MLPXOR.py
+++ Multilayer-Perceptron/MLPXOR.py
@@ -1,18 +1,1 @@
-from MLP import MLP
-import numpy as np
 
-#XOR dataset
-X = np.array([[0,0],[0,1],[1,0],[1,1]])
-y = np.array([[0], [1],[1], [0]])
-
-#train
-nn = MLP([2,2,1], lr = 0.5)
-nn.fit(X, y, epochs=20000)
-
-#test
-
-for (x, target) in zip(X, y):
-    pred = nn.predict(x)[0][0]
-    step = 1 if pred > 0.5 else 0
-    print("[Info] data={}, ground-truth={}, pred={:.4f}, step={}".format(
-        x, target[0], pred, step))
